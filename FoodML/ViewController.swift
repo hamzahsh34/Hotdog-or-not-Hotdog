32d9ddb8f66dd0de1c3acd4c27bc42a228bb0f89
hamzahs34 <hamzah_shahid@hotmail.com>
2019-08-27 20:28:27 -0400

    Obtain image recognition results back from V3 model

--- FoodML/ViewController.swift
+++ FoodML/ViewController.swift
@@ -7,14 +7,51 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = false
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = userPickedImage
+            let ciImage = CIImage(image: userPickedImage)
+            detect(image: ciImage!)
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func detect(image: CIImage) {
+        
+        let model = try? VNCoreMLModel(for: Inceptionv3().model)
+        let request = VNCoreMLRequest(model: model!) { (request, error) in
+            let results = request.results as? [VNClassificationObservation]
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        do {
+            try handler.perform([request])
+        } catch {
+            print("\(error)")
+        }
     }
 
-
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
 }
 
